27257a615865c7437d52e0ec0e05b2e43e2ee94c
Carperrr <verbaa0110@gmail.com>
2026-03-31 15:58:02 +0300

    rm payment-service add transaction service

--- apps/transaction-service/src/main.ts
+++ apps/transaction-service/src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 
-import { PaymentModule } from './payment.module';
+import { TransactionModule } from './transaction.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create(PaymentModule);
+	const app = await NestFactory.create(TransactionModule);
 	await app.listen(process.env.port ?? 3000);
 }
 void bootstrap();
